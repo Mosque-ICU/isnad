f93d7c1006f883df850ae43db45dacdd7c55db81
Aamir Adat <74106642+amdat99@users.noreply.github.com>
2023-10-30 13:53:28 +0000

    config
    config headers
    
    confg

--- next.config.js
+++ next.config.js
@@ -9,7 +9,26 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true
-  }
+  },
+  headers: [
+    // Allow for specific domains to have access or * for all
+    {
+      key: 'Access-Control-Allow-Origin',
+      value: '*'
+      // DOES NOT WORK
+      // value: process.env.ALLOWED_ORIGIN,
+    },
+    // Allows for specific methods accepted
+    {
+      key: 'Access-Control-Allow-Methods',
+      value: 'GET, POST, PUT, DELETE, OPTIONS'
+    },
+
+    {
+      key: 'Access-Control-Allow-Headers',
+      value: '*'
+    }
+  ]
 };
 
 module.exports = nextConfig;
